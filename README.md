70713f8cbd0511de4c96f507c232b15e64081311
MinZung <90918826+minza55113151@users.noreply.github.com>
2024-03-24 15:41:43 +0700

    Lottery!!!

--- README.md
+++ README.md
@@ -1,2 +1,94 @@
 # multi-party-lottery
-Lottery contract
+
+This is a multi-party lottery contract that allow multiple players to join the game and find the winner by value of the player.
+
+## Variable Field
+
+![lottery](./1.png){:height="100%" width="100%"}
+`T1` is time of stage 1.
+`T2` is time of stage 2.
+`T3` is time of stage 3.
+`playerMax` is the maximum number of players.
+`owner` is the address owner of the contract.
+`playersHash` is the hash of the player.
+`playersValue` is the value when revealed of the player.
+`playerReveal` is the reveal status of the player.
+`playerIndex` is the index of the player.
+`players` is the address of the player by index.
+`playerCount` is the number of players.
+`reward` is the reward of the game.
+`gameStartTimestamp` is the start time of the game that start when first player join.
+`isGameStart` is the status of the game.
+
+## Constructor
+
+![lottery](./2.png){:height="100%" width="100%"}
+Owner can set `T1`, `T2`, `T3`, `playerMax` in the constructor and also set the `owner` of the contract.
+
+## Reset Game State
+
+![lottery](./3.png){:height="100%" width="100%"}
+
+Reset game state by reset all the variable field to the initial state.
+
+## Game State
+
+![lottery](./4.png){:height="100%" width="100%"}
+
+This function is to get the game state of the contract that use for debugging.
+
+## Hash With Salt
+
+![lottery](./5.png){:height="100%" width="100%"}
+
+This function is to hash the player address with the salt.
+
+## Stage 1
+
+![lottery](./6.png){:height="100%" width="100%"}
+
+This function is to join the game by sending the hash of value.
+
+- Player must send the value of 0.1 ether to join the game.
+- Game must in the stage 1 to join the game.
+- Player must not exceed the maximum number of players.
+- Player must not join the game before.
+
+Then the player will be added to the game and initialize the player.
+
+## Stage 2
+
+![lottery](./7.png){:height="100%" width="100%"}
+
+This function is to reveal the value of the player.
+
+- Player must send value and salt to reveal the value.
+- Player must send value between 0 to 999
+- Game must in the stage 2 to reveal the value.
+
+Then the player will mark as revealed and the value will be stored.
+
+## Stage 3
+
+![lottery](./8.png){:height="100%" width="100%"}
+
+This function is to find the winner of the game.
+
+- Game must in the stage 3 to find the winner.
+- This function is only owner to call the function.
+
+The winner is calculate by xor all the revealed value of the player and hash then mod with the number of revealed players that will be index of winner.
+
+- If winner is found, the reward will be sent to the winner 98% and to owner 2%.
+- If no revealed player, the reward will be sent to owner.
+
+## Stage 4
+
+![lottery](./9.png){:height="100%" width="100%"}
+
+This function is to withdraw the reward of the player when end of stage 4.
+
+- Game must in the stage 4 to withdraw the reward.
+- Player must join the game before.
+
+The reward will be sent to the player then player will be mark as withdraw.
---
+++ lottery.sol
@@ -0,0 +1,134 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+contract lottery {
+    uint public T1;
+    uint public T2;
+    uint public T3;
+    uint public playerMax;
+    address public owner;
+    mapping(address => bytes32) private playersHash;
+    mapping(address => uint) private playersValue;
+    mapping(address => bool) private playersReveal;
+    mapping(address => uint) private playersIndex;
+    mapping(uint => address) private players;
+
+    uint public playerCount = 0;
+    uint public reward = 0;
+    uint public gameStartTimestamp;
+    bool public isGameStart = false;
+
+    constructor(uint _T1, uint _T2, uint _T3, uint _playerMax) {                  
+        T1 = _T1;
+        T2 = _T2;
+        T3 = _T3;
+        playerMax = _playerMax;
+        owner = msg.sender;
+    }
+
+    function resetGameState() private {
+        for (uint i=0; i<playerCount; i++) 
+        {
+            address playerAddress = players[i];
+            players[i] = address(0);
+            playersHash[playerAddress] = 0;
+            playersValue[playerAddress] = 0;
+            playersReveal[playerAddress] = false;
+            playersIndex[playerAddress] = 0;
+        }
+        playerCount = 0;
+        reward = 0;
+        isGameStart = false;
+    }
+
+    function gameState() external view returns (uint) {
+        if (isGameStart == false){
+            return 0;
+        }
+        else if (block.timestamp <= gameStartTimestamp + T1){
+            return 1;
+        }
+        else if (block.timestamp <= gameStartTimestamp + T1 + T2){
+            return 2;
+        }
+        else if (block.timestamp <= gameStartTimestamp + T1 + T2 + T3){
+            return 3;
+        }
+        else{
+            return 4;
+        }
+    }
+
+    function hashWithSalt(uint value, string memory salt) public pure returns (bytes32) {
+        require(value >= 0 && value <= 999, "Please enter value between 0 and 999");
+        return keccak256(abi.encodePacked(value, salt));
+    }
+
+    function stage1(bytes32 hash) public payable {
+        require(msg.value == 0.001 ether, "Pls send value = 0.001 ether");
+        require(!isGameStart || (block.timestamp <= gameStartTimestamp + T1), "End stage 1");
+        require(playerCount < playerMax, "Player reach max");
+        require(players[playersIndex[msg.sender]] == address(0), "You are already in the game");
+
+        reward += 0.001 ether;
+        playersHash[msg.sender] = hash;
+        playersValue[msg.sender] = 1000;
+        playersReveal[msg.sender] = false;
+        playersIndex[msg.sender] = playerCount;
+        players[playerCount] = msg.sender;
+
+        if (playerCount == 0){
+            gameStartTimestamp = block.timestamp;
+            isGameStart = true;
+        }
+
+        playerCount += 1;
+    }
+
+    function stage2(uint value, string memory salt) public {
+        require(block.timestamp > gameStartTimestamp + T1 && block.timestamp <= gameStartTimestamp + T1 + T2, "Not in stage 2");
+        require(value >= 0 && value <= 999, "Please enter value between 0 and 999");
+        require(playersHash[msg.sender] == hashWithSalt(value, salt), "Hash is not match with your reveal value");
+        playersReveal[msg.sender] = true;
+        playersValue[msg.sender] = value;
+    }
+
+    function stage3() public payable {
+        require(msg.sender == owner, "You need to be owner to call this function");
+        require(block.timestamp > gameStartTimestamp + T1 + T2 && block.timestamp <= gameStartTimestamp + T1 + T2 + T3, "Not in stage 3");
+        uint xorResult = 0;
+        uint revealPlayerCount = 0;
+        for (uint i=0; i<playerCount; i++) 
+        {
+            address playerAddress = players[i];
+            if (playersReveal[playerAddress]) {
+                xorResult ^= playersValue[playerAddress];
+                revealPlayerCount += 1;
+            }
+        }
+        if (revealPlayerCount == 0){
+            payable(owner).transfer(reward);
+        }
+        else{
+            uint result = uint(keccak256(abi.encodePacked(xorResult))) % revealPlayerCount;
+            address payable winnerAddress = payable(players[result]);
+            winnerAddress.transfer(reward * 98 / 100);
+            payable(owner).transfer(reward * 2 / 100);
+        }
+        resetGameState();
+    }
+
+    function stage4() public {
+        require(block.timestamp > gameStartTimestamp + T1 + T2 + T3, "Not in stage 4");
+        require(msg.sender == players[playersIndex[msg.sender]]);
+        assert(reward > 0 ether);
+        players[playersIndex[msg.sender]] = address(0);
+        playersIndex[msg.sender] = 0;
+        reward -= 0.001 ether;
+        playerCount -= 1;
+        payable(msg.sender).transfer(0.001 ether);
+        if (playerCount == 0){
+            resetGameState();
+        }
+    }
+}
